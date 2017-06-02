6c7374d00ddca69700869f4c569bc3d894960638
JordanFalcon <j.falcon122@gmail.com>
2017-06-02 16:17:37 -0400

    fix Issue #5 add gallery

--- gallery.js
+++ gallery.js
@@ -1,25 +1,19 @@
-function modal(image) {
-	console.log(image);
-	
 // Get the modal
-	// var modal = document.getElementById('myModal');
-
-	// Get the image and insert it inside the modal - use its "alt" text as a caption
-		// body...
-	// var img = document.getElementById('myImg');
-	// var modalImg = document.getElementById("img01");
-	// var captionText = document.getElementById("caption");
-	// img.onclick = function(){
-	//     modal.style.display = "block";
-	//     modalImg.src = this.src;
-	//     captionText.innerHTML = this.alt;
-	// }
-
-	// Get the <span> element that closes the modal
-	// var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on <span> (x), close the modal
-	// span.onclick = function() { 
-	//     modal.style.display = "none";
-	// }
+function modal(id) {
+    var modal = document.getElementById('myModal');
+    var img = document.getElementById(id);
+    var modalImg = document.getElementById("modal-img");
+        modalImg.src = img.src;
+        modal.style.display = "block";
+        img.src;
+        // captionText.innerHTML = this.alt;
+    // var span = document.getElementsByClassName("close")[0];
+    // span.onclick = function() { 
+    //     modal.style.display = "none";
+}
+function close_modal() {
+    console.log(this);
+    var modal = document.getElementById('myModal');
+    var span = document.getElementsByClassName("close")[0];
+    spand = modal.style.display = "none";
 }
