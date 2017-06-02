f13891a655910cabe0008eac42bf81557a4f1a70
JordanFalcon <j.falcon122@gmail.com>
2017-06-02 10:33:01 -0400

    slide show for index, backgound color light grey

---
+++ index.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+  var image, imageCache = [];
+  $("#slides img").each(function() {
+    image = new Image();
+        image.src = $(this).attr("src");
+        imageCache[imageCache.length] = image;
+    });
+
+  // Start slide show
+  var imageCounter = 0;
+  var nextImage, nextCaption;
+    var  timer = setInterval(
+        function () {
+          // $("#caption").fadeOut(500);
+          $("#slide").fadeOut(500,
+        function() {
+          imageCounter = (imageCounter + 1) % imageCache.length;
+              nextImage = imageCache[imageCounter];
+              // nextCaption = nextImage.title;	
+            // $("#caption").text(nextCaption).fadeIn(0);
+            $("#slide").attr("src", nextImage.src).fadeIn(500);
+        }
+          );
+        },
+      3000);
+
+});
